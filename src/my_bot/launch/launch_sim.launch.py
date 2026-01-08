faa9e84dba8aabb9865da24507e85157a514ff3e
Hrishikesh Birje <hrishikeshbirje@iitgn.ac.in>
2026-01-08 16:17:34 +0530

    control by teleop

--- src/my_bot/launch/launch_sim.launch.py
+++ src/my_bot/launch/launch_sim.launch.py
@@ -9,41 +9,46 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
 
-
-
 def generate_launch_description():
+    package_name='my_bot' 
 
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='my_bot' #<--- CHANGE ME
-
+    # 1. Robot State Publisher
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name),'launch','rsp.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
+    # 2. Gazebo Simulation
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
-                launch_arguments={'gz_args': '-r empty.sdf'}.items()
-             )
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+        launch_arguments={'gz_args': '-r empty.sdf'}.items()
+    )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    # 3. Spawn the Robot
     spawn_entity = Node(package='ros_gz_sim', executable='create',
-                        arguments=['-topic', 'robot_description',
-                                   '-name', 'my_bot', 
-                                   '-entity', 'my_bot_entity'],
-                        output='screen')
+        arguments=['-topic', 'robot_description',
+                   '-name', 'my_bot', 
+                   '-entity', 'my_bot_entity'],
+        output='screen')
 
+    # 4. The Bridge (Moved inside the function)
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
+            '/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan',
+            '/model/my_bot/odometry@nav_msgs/msg/Odometry[gz.msgs.Odometry',
+            '/model/my_bot/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V'
+        ],
+        output='screen'
+    )
 
-
-    # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        bridge, 
     ])
